ff5d887105ef6451e3c6f907b011b44ac9763b46
Jakub47 <jakub7447@gmail.com>
2018-08-10 17:32:32 +0200

    ?

---
+++ Codingame/CG/CG/Descent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CG
+{
+    class Descent
+    {
+        static void Main(string[] args)
+        {
+
+            // game loop
+            while (true)
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    int mountainH = int.Parse(Console.ReadLine()); // represents the height of one mountain.
+                }
+
+                // Write an action using Console.WriteLine()
+                // To debug: Console.Error.WriteLine("Debug messages...");
+
+                Console.WriteLine("4"); // The index of the mountain to fire on.
+            }
+        }
+    }
+}
